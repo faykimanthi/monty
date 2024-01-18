1d43c3009c05628bd31e1a5ed59aff9eb60c84e5
Faith <faykimanthi@gmail.com>
2024-01-18 15:42:49 -0800

    push

--- push.c
+++ push.c
@@ -1,4 +1,5 @@
 #include "monty.h"
+
 /**
  * f_push - pushes node to the stack
  * @head: head of stack
@@ -12,22 +13,28 @@ void f_push(stack_t **head, unsigned int counter)
 	{
 		if (bus.arg[0] == '-')
 			b++;
-		for (; bus.arg[b] != '\0'; b++)
-		{
-			if (bus.arg[b] > 57 || bus.arg[b] < 48)
-				flag = 1; }
-		if (flag == 1)
-		{ fprintf(stderr, "L%d: usage: push integer\n", counter);
-			fclose(bus.file);
-			free(bus.content);
-			free_stack(*head);
-			exit(EXIT_FAILURE); }}
-	else
-	{ fprintf(stderr, "L%d: usage: push integer\n", counter);
+	for (; bus.arg[b] != '\0'; b++)
+	{
+		if (bus.arg[b] > '9' || bus.arg[b] < '0')
+			flag = 1;
+	}
+	if (flag == 1)
+	{
+		fprintf(stderr, "L%d: usage: push integer\n", counter);
 		fclose(bus.file);
 		free(bus.content);
 		free_stack(*head);
-		exit(EXIT_FAILURE); }
+		exit(EXIT_FAILURE);
+	}
+	}
+	else
+	{
+		fprintf(stderr, "L%d: usage: push integer\n", counter);
+		fclose(bus.file);
+		free(bus.content);
+		free_stack(*head);
+		exit(EXIT_FAILURE);
+	}
 	n = atoi(bus.arg);
 	if (bus.lifi == 0)
 		addnode(head, n);
